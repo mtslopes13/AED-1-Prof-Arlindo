349deea8f2813ff01aea9ebba1bda9fe020b8bef
mtslopes13 <127984268+mtslopes13@users.noreply.github.com>
2023-06-12 15:29:06 -0300

    Update EP12.c

--- EP12.c
+++ EP12.c
@@ -1,43 +1,36 @@
 #include <stdio.h>
+#include <string.h>
 
-int calcularHash(char* linha) {
-    int i, hash = 0;
-    for (i = 0; linha[i] != '\0'; i++) {
-        int posicaoAlfabeto = linha[i] - 'A';
-        int elemento = 0;
-        int posicao = i;
-        int valor = posicaoAlfabeto + elemento + posicao;
-        hash += valor;
+int calcularHash(char *string) {
+    int hash = 0;
+    int tamanho = strlen(string);
+    
+    for (int i = 0; i < tamanho; i++) {
+        int posicaoAlfabeto = string[i] - 'A';
+        hash += posicaoAlfabeto + i + strlen(string);
     }
+    
     return hash;
 }
 
 int main() {
     int casosTeste;
     scanf("%d", &casosTeste);
-
-    while (casosTeste > 0) {
+    
+    for (int i = 0; i < casosTeste; i++) {
         int numLinhas;
         scanf("%d", &numLinhas);
-
-        int i, j;
-        for (i = 0; i < numLinhas; i++) {
-            char linha[51];
-            scanf("%s", linha);
-
-            int hash = 0;
-            for (j = 0; linha[j] != '\0'; j++) {
-                int posicaoAlfabeto = linha[j] - 'A';
-                int elemento = i;
-                int posicao = j;
-                int valor = posicaoAlfabeto + elemento + posicao;
-                hash += valor;
-            }
-            printf("%d\n", hash);
+        
+        int totalHash = 0;
+        
+        for (int j = 0; j < numLinhas; j++) {
+            char string[51];
+            scanf("%s", string);
+            totalHash += calcularHash(string);
         }
-
-        casosTeste--;
+        
+        printf("%d\n", totalHash);
     }
-
+    
     return 0;
 }
