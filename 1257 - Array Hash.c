75c2f678b2fba6cb854a8f982b3a3b6c13ab46ba
mtslopes13 <127984268+mtslopes13@users.noreply.github.com>
2023-06-26 18:21:41 -0300

    Create 1257 - Array Hash.c

---
+++ 1257 - Array Hash.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+int calcularHash(char *string) {
+    int hash = 0;
+    int tamanho = strlen(string);
+    
+    for (int i = 0; i < tamanho; i++) {
+        int posicaoAlfabeto = string[i] - 'A';
+        hash += posicaoAlfabeto + i + 1;
+    }
+    
+    return hash;
+}
+
+int main() {
+    int casosTeste;
+    scanf("%d", &casosTeste);
+    
+    for (int i = 0; i < casosTeste; i++) {
+        int numLinhas;
+        scanf("%d", &numLinhas);
+        
+        int totalHash = 0;
+        
+        for (int j = 0; j < numLinhas; j++) {
+            char string[51];
+            scanf("%s", string);
+            totalHash += calcularHash(string);
+        }
+        
+        printf("%d\n", totalHash);
+    }
+    
+    return 0;
+}
